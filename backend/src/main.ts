84f028354f55cb7b578c965de4a2b3efc78070c9
Vickash <vickash@Vickashs-MacBook-Air.local>
2026-08-15 02:09:23 +0100

    Configure backend for DigitalOcean

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -49,7 +49,7 @@ async function bootstrap(): Promise<void> {
     process.env.PORT ?? 3000,
   );
 
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 
   console.log(
     `TenureEx API running on http://localhost:${port}/api/v1`,
